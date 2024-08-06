4edecbbbc2cbaac82c016639ef3ed12782238bcb
Bogdan252525 <bogdan2516@gmail.com>
2024-08-06 18:32:35 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  const stack = [];
+  const bracketsMap = {};
+  const openBrackets = new Set();
+	
+  for (const [open, close] of bracketsConfig) {
+    bracketsMap[close] = open;
+    openBrackets.add(open);
+  }
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (bracketsMap[char] === char) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
